b80d37f2243e514adce3f06d2c092f59b9cb5d50
harshitkumaragarwal <agarwalharshit063@gmail.com>
2026-01-29 11:31:46 +0530

    question 4

---
+++ question4.py
@@ -0,0 +1,11 @@
+num1=(input("enter a number"))
+num2=(input("enter a number"))
+
+if num1>num2:
+    print("num1 is larger than num2")
+    if num2>num1:
+        print("num2 is larger than num1")
+        if num1==num2:
+            print("both are equal")
+        else:
+            print("0")
