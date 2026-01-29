27ed37f8535023a0a892da43560ff8fcf0a5dc35
harshitkumaragarwal <agarwalharshit063@gmail.com>
2026-01-29 19:24:29 +0530

    the next date python

---
+++ question6.py
@@ -0,0 +1,10 @@
+date=(input("enter a date "))
+month=(input("enter a month"))
+year=(input("enter a year"))
+
+date =int(date)
+date=date+1
+
+print("the next date is",date)
+print("the month is ",month)
+print("the year is",year)
